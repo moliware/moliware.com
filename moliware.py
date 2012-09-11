258934590079846866a4d2f7fa1a2aa107e6f7e2
moliware <miguel@moliware.com>
2012-09-11 16:34:04 +0200

    Added flags in the navigation bar

--- moliware.py
+++ moliware.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 from flaskext.babel import Babel
 
 import os
+
+
+ACCEPTED_LANGS = ['en', 'es']
+DEFAULT_LANG = 'en'
 
 
 app = Flask(__name__)
@@ -11,14 +15,19 @@ babel = Babel(app)
 
 @babel.localeselector
 def get_locale():
-    return request.accept_languages.best_match(['es', 'en']) or 'en'
+    if 'lang' in request.args:
+        lang = request.args.get('lang')
+        if lang in ACCEPTED_LANGS:
+            return lang
+    return request.accept_languages.best_match(ACCEPTED_LANGS) or DEFAULT_LANG
 
 
 @app.route('/')
-def index():
+def index(lang=None):
     return render_template('index.html')
 
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    debug = 'DEBUG' in os.environ
+    app.run(host='0.0.0.0', port=port, debug=debug)
